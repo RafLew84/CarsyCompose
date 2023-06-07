2278dea7c48ddc14d61a8c29a21d23e8253a5704
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-06-07 18:48:23 +0200

    TimeLine Month composable

--- app/src/main/java/com/example/carsycompose/ui/screens/TimeLineScreen.kt
+++ app/src/main/java/com/example/carsycompose/ui/screens/TimeLineScreen.kt
@@ -1,16 +1,36 @@
 package com.example.carsycompose.ui.screens
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Circle
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.carsycompose.data.CostListItem
 import com.example.carsycompose.data.DataProvider
 
@@ -18,7 +38,9 @@ import com.example.carsycompose.data.DataProvider
 fun TimeLineScreen(paddingValues: PaddingValues){
     val data = remember { DataProvider.getTimeLineList(DataProvider.cars[0].costs) }
     LazyColumn(
-        modifier = Modifier.padding(paddingValues).fillMaxSize(),
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -33,15 +55,49 @@ fun TimeLineScreen(paddingValues: PaddingValues){
 }
 @Composable
 private fun YearItem(item: CostListItem.CostYearItem){
-    Text(text = item.year)
+    Text(
+        text = item.year,
+        fontSize = 36.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Cyan,
+        modifier = Modifier
+            .height(66.dp)
+            .padding(top = 12.dp)
+    )
 }
 
 @Composable
 private fun MonthItem(item: CostListItem.CostMonthItem){
-    Text(text = item.month)
+    Row(
+        modifier = Modifier.fillMaxWidth().height(40.dp)
+    ){
+        Box(modifier = Modifier
+            .width(60.dp)
+            .fillMaxHeight()
+            .drawWithContent {
+                drawContent()
+                drawLine(
+                    color = Color(128, 203, 196),
+                    start = Offset(size.width / 2, 0f),
+                    end = Offset(size.width / 2, size.height),
+                    strokeWidth = 4.dp.toPx()
+                )
+            }
+        ) {
+            Image(
+                imageVector = Icons.Default.Circle,
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth().padding(top = 6.dp),
+                colorFilter = ColorFilter.tint(Color(128, 203, 196))
+            )
+
+        }
+        Text(text = item.month, color = Color(128, 203, 196), fontSize = 24.sp)
+    }
+
 }
 
 @Composable
 private fun GeneralItem(item: CostListItem.CostGeneralItem){
-    Text(text = item.cost.type.costType)
+        Text(text = item.cost.type.costType)
 }
