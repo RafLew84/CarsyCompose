8f300c6ea61d086b3f785c65ee480a19aef40b40
Raf Lew <82097814+RafLew84@users.noreply.github.com>
2023-06-08 18:39:53 +0200

    timeline design

--- app/src/main/java/com/example/carsycompose/ui/screens/TimeLineScreen.kt
+++ app/src/main/java/com/example/carsycompose/ui/screens/TimeLineScreen.kt
@@ -1,11 +1,10 @@
 package com.example.carsycompose.ui.screens
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,11 +13,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Circle
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -33,6 +32,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.carsycompose.data.CostListItem
 import com.example.carsycompose.data.DataProvider
+import com.example.carsycompose.util.dateFormatter
+import com.example.carsycompose.util.decimalFormat
 
 @Composable
 fun TimeLineScreen(paddingValues: PaddingValues){
@@ -59,7 +60,7 @@ private fun YearItem(item: CostListItem.CostYearItem){
         text = item.year,
         fontSize = 36.sp,
         fontWeight = FontWeight.Bold,
-        color = Color.Cyan,
+        color = Color(128, 203, 196),
         modifier = Modifier
             .height(66.dp)
             .padding(top = 12.dp)
@@ -69,7 +70,9 @@ private fun YearItem(item: CostListItem.CostYearItem){
 @Composable
 private fun MonthItem(item: CostListItem.CostMonthItem){
     Row(
-        modifier = Modifier.fillMaxWidth().height(40.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(40.dp)
     ){
         Box(modifier = Modifier
             .width(60.dp)
@@ -87,17 +90,80 @@ private fun MonthItem(item: CostListItem.CostMonthItem){
             Image(
                 imageVector = Icons.Default.Circle,
                 contentDescription = "",
-                modifier = Modifier.fillMaxWidth().padding(top = 6.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 6.dp),
                 colorFilter = ColorFilter.tint(Color(128, 203, 196))
             )
 
         }
-        Text(text = item.month, color = Color(128, 203, 196), fontSize = 24.sp)
+        Text(
+            text = item.month,
+            color = Color(166, 111, 131),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 
 }
 
 @Composable
 private fun GeneralItem(item: CostListItem.CostGeneralItem){
-        Text(text = item.cost.type.costType)
+    Row(
+        modifier = Modifier.height(68.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(modifier = Modifier
+            .width(60.dp)
+            .fillMaxHeight(),
+            contentAlignment = Alignment.Center
+        ) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawLine(
+                    color = Color(128, 203, 196),
+                    start = Offset(size.width / 2, 0f),
+                    end = Offset(size.width / 2, size.height),
+                    strokeWidth = 4.dp.toPx()
+                )
+            }
+            Image(
+                imageVector = Icons.Default.Circle,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(48.dp)
+                    .fillMaxWidth()
+                    .padding(top = 6.dp),
+                colorFilter = ColorFilter.tint(Color(128, 203, 196))
+            )
+
+            Image(
+                imageVector = item.cost.type.icon,
+                contentDescription = "",
+                modifier = Modifier
+                    .size(24.dp)
+                    .padding(top = 4.dp)
+            )
+
+        }
+        Column(Modifier) {
+            Text(
+                text = item.cost.type.costType,
+                fontSize = 20.sp,
+                color = Color(128, 203, 196)
+            )
+            Text(
+                text = item.cost.date.format(dateFormatter),
+                fontSize = 12.sp,
+                color = Color(128, 203, 196)
+            )
+        }
+        Spacer(Modifier.weight(1f))
+        Text(
+            text = item.cost.amount.toString().format(decimalFormat) + " zł",
+            modifier = Modifier.align(Alignment.CenterVertically).padding(end = 12.dp),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(128, 203, 196)
+        )
+    }
 }
